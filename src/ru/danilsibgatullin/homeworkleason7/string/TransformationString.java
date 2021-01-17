b5f0dcb52287ad599001211a6a144b487ef7d1ad
DanilSibgatullin <danilsibgatullin@gmail.com>
2021-01-17 20:19:36 +0300

    Refactor StringtransformationString

--- src/ru/danilsibgatullin/homeworkleason7/string/TransformationString.java
+++ src/ru/danilsibgatullin/homeworkleason7/string/TransformationString.java
@@ -13,12 +13,12 @@ public class TransformationString {
         System.out.println("Последний символ : " + str.charAt(str.length()-1));
         System.out.println("Строка заканчивается на !!! : " + str.endsWith("!!!"));
         System.out.println("Строка начинается на I like : " + str.startsWith("I like"));
-        System.out.println("Строка содержит Java : " + str.contains(find));
-        System.out.println("Позиция подстроки Java : " + str.indexOf(find));
+        System.out.println("Строка содержит "+  find+" : " + str.contains(find));
+        System.out.println("Позиция подстроки "+  find+" : " + str.indexOf(find));
         System.out.println("Замена буквы а на о : " + str.replace('a','o'));
         System.out.println("Преобразование строки в верхний регистр : " + str.toUpperCase());
         System.out.println("Преобразование строки в нижний регистр : " + str.toLowerCase());
-        System.out.println("Вырезание строки Java : " + str.substring(0,str.indexOf(find))+str.substring(str.indexOf(find) + find.length(),str.length()));
+        System.out.println("Вырезание строки "+  find+" : " + str.substring(0,str.indexOf(find))+str.substring(str.indexOf(find) + find.length(),str.length()));
     }
 
 }
